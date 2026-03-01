01e9a51436dc6fc13d9e462f98f9f622965d3608
Michel NAUD <michel.naud26400@gmail.com>
2026-03-01 14:34:08 +0100

    fix public assets

--- frontend/src/components/home/HeroBanner.vue
+++ frontend/src/components/home/HeroBanner.vue
@@ -2,14 +2,14 @@
   <section class="page-container pt-6 mb-6">
     <div class="bg-white shadow-lg rounded-lg p-4">
       <img
-        src="/img/test_banniere_1.jpg"
+        src="/img/test_banniere_1.webp"
         alt="VEAF Banner"
         class="w-full rounded-lg"
       />
       <div class="py-8 text-center">
         <p class="text-gray-600 text-lg mb-4">Bienvenue sur le site Internet de la</p>
         <img
-          src="/img/Logo_veaf.png"
+          src="/img/Logo_veaf.webp"
           alt="Logo VEAF"
           class="mx-auto mb-4 h-16 md:h-20"
         />
