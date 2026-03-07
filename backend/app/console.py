5e3d9024adbeb92617638313b309886a2935258d
Michel NAUD <michel.naud26400@gmail.com>
2026-03-07 13:06:21 +0100

    add email cli

--- backend/app/console.py
+++ backend/app/console.py
@@ -2,12 +2,14 @@ import typer
 
 from app.commands.calendar import calendar_app
 from app.commands.database import database_app
+from app.commands.email import email_app
 from app.commands.maintenance import maintenance_app
 from app.commands.teamspeak import teamspeak_app
 
 app = typer.Typer(help="VEAF Website CLI", no_args_is_help=True)
 app.add_typer(calendar_app, name="calendar")
 app.add_typer(database_app, name="database")
+app.add_typer(email_app, name="email")
 app.add_typer(maintenance_app, name="maintenance")
 app.add_typer(teamspeak_app, name="teamspeak")
 
